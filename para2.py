cb0784ae91ecd5e77eb7a1f4c74ab999ec241897
washington <washingtonrissarodrigues@gmail.com>
2024-08-28 18:55:53 -0300

    Laço de repetição

---
+++ para2.py
@@ -0,0 +1,19 @@
+alunos = [
+    "Connor",
+    "Cj",
+    "Paulinha",
+    "Red Beramar",
+    "Albara",
+    "Biro",
+    "Bagual",
+    "Moises",
+    "Kaio",
+    "Ri fdp"
+]
+alunos.sort()
+alunos.append("Giovanna")
+alunos.reverse()
+y = 1
+for x in alunos:
+    print(y,"-",x)
+    y = y + 1
